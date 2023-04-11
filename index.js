9e40076512ece3e3edc1e636e30f08102f01431a
Ren Blake <rcblake06@gmail.com>
2023-04-10 21:25:58 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 1
+let num2 = 62
 
-// Write your code here
+let multiply = num1*num2
+
+let random = Math.floor(Math.random()*10)+1
+
+let num3 = 24
+let num4 = 5
+
+let mod = num3 % num4
+
+let max = Math.max(1,5,20)
